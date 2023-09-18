213e569221b26e0744d097c0b63faf83e35efc15
chenmic8 <chenmic8@msu.edu>
2023-09-18 18:15:14 -0400

    fill in landing page information

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,40 +1,51 @@
 import Header from "../components/Header";
 import ProjectCard from "../components/ProjectCard";
 
+const landingPage = {
+  title: "<Programmer>",
+  text: "Developer who writes clean, elegant and efficient code",
+  subtext:
+    "I'm a Full Stack web developer based in Troy, Michigan. Since 2022, I've enjoyed turning complex problems into simple, beautiful and intuitive designs. When I'm not pushing pixels, you'll find me rock climbing, pumping metal, or playing video games.",
+  titleImage:
+    "https://c4.wallpaperflare.com/wallpaper/480/585/181/iu-k-pop-hd-wallpaper-preview.jpg",
+};
+
+const landingProjects = [
+  {
+    title: "Programming Startup Simulator",
+    description: "Idle game",
+    image: "https://www.celoxis.com/cassets/img/pmc/project-management.png",
+  },
+  {
+    title: "Project Management",
+    description: "Side project",
+    image: "https://www.celoxis.com/cassets/img/pmc/project-management.png",
+  },
+  {
+    title: "Car Share",
+    description: "Side project",
+    image: "https://www.celoxis.com/cassets/img/pmc/project-management.png",
+  },
+];
+
 const Landing = () => {
   return (
     <>
       <Header
-        title={"Landing"}
-        text={"text"}
-        subtext={"subtext"}
-        image='https://c4.wallpaperflare.com/wallpaper/480/585/181/iu-k-pop-hd-wallpaper-preview.jpg'
+        title={landingPage.title}
+        text={landingPage.text}
+        subtext={landingPage.subtext}
+        image={landingPage.titleImage}
       />
-      <div className="flex flex-row px-20 py-16 gap-5">
-        <ProjectCard
-          link={"https://www.adhamdannaway.com/"}
-          image={
-            "https://www.celoxis.com/cassets/img/pmc/project-management.png"
-          }
-          title={"Project Title"}
-          description={"short project description"}
+      <div className='flex flex-row px-20 py-16 gap-5'>
+        {landingProjects.map((project) => (
+          <ProjectCard
+            link={"https://www.adhamdannaway.com/"}
+            image={project.image}
+            title={project.title}
+            description={project.description}
           />
-        <ProjectCard
-          link={"https://www.adhamdannaway.com/"}
-          image={
-            "https://www.celoxis.com/cassets/img/pmc/project-management.png"
-          }
-          title={"Project Title"}
-          description={"short project description"}
-        />
-        <ProjectCard
-          link={"https://www.adhamdannaway.com/"}
-          image={
-            "https://www.celoxis.com/cassets/img/pmc/project-management.png"
-          }
-          title={"Project Title"}
-          description={"short project description"}
-        />
+        ))}
       </div>
     </>
   );
